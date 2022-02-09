52025afe18e0ce5a19a12a77c1fe4030696a03c9
thilini-anuradha <pttanuradha@std.appsc.sab.ac.lk>
2022-02-09 18:17:53 +0530

    initialCommit

--- evisfarm/src/Pages/Login.js
+++ evisfarm/src/Pages/Login.js
@@ -7,20 +7,19 @@ function Login()
 {
     return (
           <div className="Log">
+              <h2>Welcomw to Evisfarm !</h2>
              <form> 
                 <label> <p>Username</p> <input type="text" /> </label>
                 <label> <p>Password</p> <input type="password" /> </label>
-             <div>
-                <button type="submit">Submit</button>
-             </div>
-            </form>
-               
-                   <Link to="/Profile"><button> USER </button></Link>
+                 <div>
+                   <Link to="/Profile"><button> Submit </button></Link>
                    <Router>
                    <Switch> 
                      <Route path="/Profile" component={Profile} /> 
                    </Switch> 
                    </Router>
+                   </div>
+                   </form>
           </div>
     );
 }
