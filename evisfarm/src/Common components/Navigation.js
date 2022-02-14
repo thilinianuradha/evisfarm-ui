27493aa5658883ad857f8880da42fd3b8bc233d6
thilini-anuradha <pttanuradha@std.appsc.sab.ac.lk>
2022-02-14 19:42:33 +0530

    Navigation bar added

--- evisfarm/src/Common components/Navigation.js
+++ evisfarm/src/Common components/Navigation.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+
 import { Navbar , Nav } from 'react-bootstrap'
+import { Form , FormControl, Button } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
 
 export default class Navigation extends Component {
   render() {
     return (
-      <Navbar bg='light' expand='lg'>
-          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-          <Navbar.Collapse id='basic-navbar-nav'>
-
-          <Nav>
-              <NavLink className='d-inline p-2 bg-light text-black' to="/">Home</NavLink>
-              <NavLink className='d-inline p-2 bg-light text-black' to="Profile">profile</NavLink>
-              <NavLink className='d-inline p-2 bg-light text-black' to="/Login">Login</NavLink>
-          </Nav>
+        <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">EvisFarm</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="">Market Place</Nav.Link>
+              <Nav.Link href="">About Us</Nav.Link>
+              <Nav.Link href="">Contact Us</Nav.Link>
+        
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
           </Navbar.Collapse>
+        </Container>
       </Navbar>
     )
   }
